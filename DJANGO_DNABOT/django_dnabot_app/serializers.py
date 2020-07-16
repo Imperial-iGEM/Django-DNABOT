d67a6c8b42977d445ebbecbfd450fec86d3f5031
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 12:36:22 +0200

    updating serializers

--- DJANGO_DNABOT/django_dnabot_app/serializers.py
+++ DJANGO_DNABOT/django_dnabot_app/serializers.py
@@ -4,7 +4,4 @@ from rest_framework import serializers
 class InputSerializer(serializers.Serializer):
     class Meta:
         model = Input
-        fields = ['ethanol_stage2',
-                  'deep_well_stage4',
-                  'construct_csv',
-                  'parts_linkers_csv']
+        fields = ['id', 'ethanol_stage2', 'deep_well_stage4', 'construct_csv', 'parts_linkers_csv']
---
+++ DJANGO_DNABOT/django_dnabot_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django_dnabot_app import views
+
+urlpatterns = [
+#    path('/', views.snippet_list),
+#    path('snippets/<int:pk>/', views.snippet_detail),
+]
