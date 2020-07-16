e48596656694eb8765429d4eed2e85e218d27afd
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 12:01:32 +0200

    add basic urls

--- DJANGO_DNABOT/DJANGO_DNABOT/urls.py
+++ DJANGO_DNABOT/DJANGO_DNABOT/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import include, path
+from rest_framework import routers
+from django_dnabot_app import views
+
+router = routers.DefaultRouter()
+router.register(r'Input', views.InputViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
