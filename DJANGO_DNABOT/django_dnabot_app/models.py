3238786212b3541e35520f180296ef124d133181
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 13:47:39 +0200

    files upload to files folder

--- DJANGO_DNABOT/django_dnabot_app/models.py
+++ DJANGO_DNABOT/django_dnabot_app/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Input(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     ethanol_stage2 = models.CharField(max_length=100)
     deep_well_stage4 = models.CharField(max_length=100)
-    construct_csv = models.FileField(blank=False)
-    parts_linkers_csv = models.FileField(blank=False)
+    construct_csv = models.FileField(upload_to="files", blank=False)
+    parts_linkers_csv = models.FileField(upload_to="files", blank=False)
     python_output_1 = models.FileField(blank=True)
     python_output_2 = models.FileField(blank=True)
     python_output_3 = models.FileField(blank=True)
