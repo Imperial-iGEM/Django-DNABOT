6b4cb5c22619029407e014f8fbdb1039f99bcc4a
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 12:48:25 +0200

    updated URLS

--- DJANGO_DNABOT/django_dnabot_app/urls.py
+++ DJANGO_DNABOT/django_dnabot_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django_dnabot_app import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-#    path('/', views.snippet_list),
-#    path('snippets/<int:pk>/', views.snippet_detail),
+    path('Inputs/', views.InputList.as_view()),
+    path('Inputs/<int:pk>/', views.InputDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
