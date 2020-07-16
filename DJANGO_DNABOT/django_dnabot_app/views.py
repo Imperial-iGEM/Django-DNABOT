5f7b2521cad92cc084ae5fa7d13b02ad46d465fa
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 14:51:16 +0200

    updated views for a custom create

--- DJANGO_DNABOT/django_dnabot_app/views.py
+++ DJANGO_DNABOT/django_dnabot_app/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, generics
 from .serializers import InputSerializer
 
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Input
 
 # Create your views here.
@@ -11,3 +14,17 @@ from .models import Input
 class InputView(viewsets.ModelViewSet):
     queryset = Input.objects.all()
     serializer_class = InputSerializer
+
+    def create(self, request, *args, **kwargs):
+        input_data = request.data
+        new_input = Input.objects.create(
+            ethanol_stage2=input_data["ethanol_stage2"],
+            deep_well_stage4=input_data["deep_well_stage4"],
+            construct_csv=input_data["construct_csv"],
+            parts_linkers_csv=input_data["parts_linkers_csv"])
+
+        new_input.save()
+
+        serializer = InputSerializer(new_input)
+        return Response(serializer.data)
+        
