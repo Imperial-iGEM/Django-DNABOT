3d9e2ac0eb53a2d5e2e01808a99dd87c5dc0a1fa
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 13:01:04 +0200

    following pretty printed tutorial

--- DJANGO_DNABOT/DJANGO_DNABOT/urls.py
+++ DJANGO_DNABOT/DJANGO_DNABOT/urls.py
@@ -14,16 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
 from django.urls import include, path
 from rest_framework import routers
 from django_dnabot_app import views
 
-router = routers.DefaultRouter()
-router.register(r'Input', views.InputViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('django_dnabot_app.urls')),
     path('admin/', admin.site.urls),
 ]
