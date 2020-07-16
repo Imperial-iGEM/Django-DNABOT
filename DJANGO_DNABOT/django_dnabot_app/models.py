fc01ab54146e318af641d1f02c185149de4de30c
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 12:11:50 +0200

    add first attempt guess at models

--- DJANGO_DNABOT/django_dnabot_app/models.py
+++ DJANGO_DNABOT/django_dnabot_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Input(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    ethanol_stage2 = models.CharField(max=100)
+    deep_well_stage4 = models.CharField(max=100)
+    construct_csv = models.FileField(upload_to="/",blank=False)
+    parts_linkers_csv = models.FileField(upload_to="/",blank=False)
+    python_output_1 = models.FileField(upload_to="/",blank=True)
+    python_output_2 = models.FileField(upload_to="/",blank=True)
+    python_output_3 = models.FileField(upload_to="/",blank=True)
+    python_output_4 = models.FileField(upload_to="/",blank=True)
+
+    class Meta:
+        ordering = ['created']
