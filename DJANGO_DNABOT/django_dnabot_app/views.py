c034fb9d778197278eb40137a944ca2bd4e1a53d
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 11:56:42 +0200

    adds basic views.py

--- DJANGO_DNABOT/django_dnabot_app/views.py
+++ DJANGO_DNABOT/django_dnabot_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from django_dnabot_app.serializers import InputSerializer
 
 # Create your views here.
+
+class InputViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = InputSerializer
