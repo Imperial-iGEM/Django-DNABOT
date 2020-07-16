ce77f93aa8c01d2b59695e4a1409ac2d378693f3
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-07-16 13:50:31 +0200

    adds visibility to python outputs

--- DJANGO_DNABOT/django_dnabot_app/serializers.py
+++ DJANGO_DNABOT/django_dnabot_app/serializers.py
@@ -5,5 +5,13 @@ from rest_framework import serializers
 class InputSerializer(serializers.ModelSerializer):
     class Meta:
         model = Input
-        fields = ['id', 'ethanol_stage2', 'deep_well_stage4',
-                  'construct_csv', 'parts_linkers_csv']
+        fields = ['id',
+                  'ethanol_stage2',
+                  'deep_well_stage4',
+                  'construct_csv',
+                  'parts_linkers_csv',
+                  'python_output_1',
+                  'python_output_2',
+                  'python_output_3',
+                  'python_output_4',
+                  ]
